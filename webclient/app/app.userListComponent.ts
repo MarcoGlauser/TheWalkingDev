1124330caff7f50f7526505af82fb8d040a15c9f
mglauser <marco.glauser@gmail.com>
2016-09-17 16:40:23 +0200

    added sorting

--- webclient/app/app.userListComponent.ts
+++ webclient/app/app.userListComponent.ts
@@ -5,7 +5,7 @@ import { User } from './models/user';
 @Component({
     selector: 'my-item-component',
     providers: [DataService],
-    template: '<div *ngFor="let user of myItems">{{ user.username }} took {{user.total_steps}} steps</div>',
+    template: '<div *ngFor="let user of myItems">{{ user.username }} took {{user.total_steps}} steps</div>'
 })
 
 export class MyItemComponent implements OnInit {
@@ -20,7 +20,13 @@ export class MyItemComponent implements OnInit {
     private getAllItems(): void {
         this._dataService
             .GetAll()
-            .subscribe((data:User[]) => this.myItems = data,
+            .subscribe((data:User[]) => {
+                data.sort((a:User,b:User) =>{
+                    return a.total_steps - b.total_steps
+                })
+                data.reverse()
+                this.myItems = data
+                },
                 error => console.log(error),
                 () => console.log('Get all Items complete'));
     }
