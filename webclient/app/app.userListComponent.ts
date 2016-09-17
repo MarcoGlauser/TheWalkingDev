53be41eea18cd5bbf34b64c281c71d79151802b5
mglauser <marco.glauser@gmail.com>
2016-09-17 23:18:14 +0200

    ui shit

--- webclient/app/app.userListComponent.ts
+++ webclient/app/app.userListComponent.ts
@@ -7,14 +7,49 @@ import {StepDiff} from "./models/step_diff";
     selector: 'my-item-component',
     providers: [DataService],
     template: `
-        <div *ngFor="let user of myItems | orderBy : '-total_steps' ">
-            {{ user.username }} took {{user.total_steps}} steps
-        </div>
-        <div *ngIf="activeUsers && activeUsers.length > 0">
-            <div *ngFor="let user of activeUsers | orderBy : '-total_steps' ">
-            {{ user.username }},
+        <div class="container">
+            <div class="row">
+                <div id="generalStats" class="col-md-6" >
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="center-block text-center bs-callout bs-callout-primary"><h4>{{totalSteps}}</h4> <span class="subdesc">Total steps</span></div>
+                        </div>
+                        <div class="row">
+                            <div class="center-block text-center bs-callout bs-callout-primary"><h4>1337</h4> <span class="subdesc">steps Today</span></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-1">
+                </div>
+                
+                <div id="ranking" class="col-md-5 bs-callout bs-callout-info col-md-offset-9">
+                    <div *ngFor="let user of myItems | orderBy : '-total_steps' " >
+                        <div class="text-left subdesc">
+                            {{ user.username }}
+                        </div>
+                        <div class="progress">
+                            <div class="progress-bar progress-bar-success  " [ngClass]="{'active': !activeIds.indexOf(user.id),'progress-bar-striped':!activeIds.indexOf(user.id)}" role="progressbar" [ngStyle]="{'width': user.step_percentage+'%'}" >
+                                {{ user.total_steps }}
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
             </div>
-            Are currently Walking around
+            <div class="row">
+                <div class="center-block text-center bs-callout bs-callout-success" *ngIf="activeUsers && activeUsers.length > 0">
+                    
+                    <div class="text-center pull-left person" *ngFor="let user of activeUsers | orderBy : '-total_steps' ">
+                        <img class="person_icon" src="../images/person_icon.png">
+                        <div>{{ user.username }}</div>
+                    </div>
+                    <div class="clearfix"></div>
+                    <span *ngIf="activeUsers.length > 1">Are awesome and walking around</span>
+                    <span *ngIf="activeUsers.length < 2">Is being mindful of his health and walking around</span>
+                    
+                </div>
+                <div class="text-center bs-callout bs-callout-danger" *ngIf="!activeUsers || activeUsers.length == 0">Everyone is sitting on their asses. Check back later.</div>
+            </div>
         </div>
         `
 })
@@ -23,25 +58,37 @@ export class MyItemComponent implements OnInit {
     public myItems: User [] = [];
     public activeUsers: User [] = [];
     public activeIds:number[] = [];
+    public totalSteps:number = 0;
 
     constructor(private _dataService: DataService) { }
 
     ngOnInit() {
         this.getAllItems();
-        setInterval(() => {this.getActiveUsers();}, 3000);
+        setInterval(() => {this.getActiveUsers();}, 2000);
 
     }
 
     private updateUsers(): void {
+        let self = this;
         for(let user of this.myItems){
             this._dataService
                 .GetSingle(user.id)
                 .subscribe((data:User) => {
+                        let delta = data.total_steps - user.total_steps
+                        self.totalSteps += delta;
                         user.total_steps = data.total_steps
                     },
                     error => console.log(error),
                     () => console.log('Get all Items complete'));
         }
+        for(let user of this.myItems) {
+            this.calculateStepPercentage(user)
+        }
+    }
+
+    private calculateStepPercentage(user:User): void{
+        user.step_percentage = Math.round(user.total_steps/this.totalSteps*100)
+        console.log(user.step_percentage)
     }
 
     private getAllItems(): void {
@@ -50,11 +97,14 @@ export class MyItemComponent implements OnInit {
             .GetAll()
             .subscribe((data:User[]) => {
                 this.myItems = data
+                for(let user of data){
+                    self.totalSteps += user.total_steps
+                }
                 },
                 error => console.log(error),
                 () => {
                     console.log('Get all Items complete')
-                    setInterval(() => { self.updateUsers()},5000)
+                    setInterval(() => { self.updateUsers()},2000)
                 });
     }
 
