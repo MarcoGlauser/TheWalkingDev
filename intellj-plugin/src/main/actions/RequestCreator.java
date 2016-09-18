09b2952032d85dcd2ffccf2717ec032c5c5a52bf
Kirusanth Poopalasingam <pkirusanth@gmail.com>
2016-09-19 00:27:54 +0200

    fix

--- intellj-plugin/src/main/actions/RequestCreator.java
+++ intellj-plugin/src/main/actions/RequestCreator.java
@@ -1,17 +1,31 @@
 package main.actions;
 
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.auth.oauth2.RefreshTokenRequest;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.util.Utils;
 import com.google.api.client.http.apache.ApacheHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.fitness.Fitness;
 import com.google.api.services.fitness.model.*;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.jetbrains.annotations.NotNull;
 import org.joda.time.DateTime;
 
+import javax.net.ssl.SSLContext;
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.Collections;
 
 /**
@@ -20,17 +34,71 @@ import java.util.Collections;
 public class RequestCreator {
 
     private final Fitness service;
+    private static String accessToken;
 
     public RequestCreator() {
         service = createService();
     }
 
-    public Integer sendRequest() {
+    private static CloseableHttpClient unsafeHttpClient;
+
+    static {
         try {
-            return careless();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustSelfSignedStrategy() {
+                public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                    return true;
+                }
+            }).build();
+
+            unsafeHttpClient = HttpClients.custom().setSslcontext(sslContext)
+                .setSSLHostnameVerifier(new NoopHostnameVerifier()).build();
+
+        } catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException e) {
+            e.printStackTrace();
         }
+    }
+
+    public static CloseableHttpClient getClient() {
+        return unsafeHttpClient;
+    }
+
+    public Integer sendRequest() {
+        Unirest.setHttpClient(unsafeHttpClient);
+
+        DateTime startTime = DateTime.now().minusDays(1);
+        DateTime endTime = DateTime.now().plusDays(1);
+
+        try {
+            HttpResponse<JsonNode> authorization = Unirest.post("https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate")
+                .header("Content-Type", "application/json;encoding=utf-8")
+                .header("Authorization", "Bearer " + accessToken)
+                .body("{\n" +
+                    "  \"aggregateBy\": [{\n" +
+                    "    \"dataTypeName\": \"com.google.step_count.delta\",\n" +
+                    "    \"dataSourceId\": \"derived:com.google.step_count.delta:com.google.android.gms:estimated_steps\"\n" +
+                    "  }],\n" +
+                    "  \"bucketByTime\": { \"durationMillis\": " + endTime.minus(startTime.getMillis()).getMillis() + " },\n" +
+                    "  \"startTimeMillis\": " + startTime.getMillis() + ",\n" +
+                    "  \"endTimeMillis\": " + endTime.getMillis() + "\n" +
+                    "}")
+                .asJson();
+            int value = authorization.getBody()
+                .getObject()
+                .getJSONArray("bucket")
+                .getJSONObject(0)
+                .getJSONArray("dataset")
+                .getJSONObject(0)
+                .getJSONArray("point")
+                .getJSONObject(0)
+                .getJSONArray("value")
+                .getJSONObject(0)
+                .getInt("intVal");
+            return value;
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+
+        return 0;
     }
 
     private Integer careless() throws IOException {
@@ -73,6 +141,8 @@ public class RequestCreator {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        accessToken = applicationDefault.getAccessToken();
 
         return new Fitness.Builder(Utils.getDefaultTransport(), Utils.getDefaultJsonFactory(), applicationDefault)
             .setApplicationName("ItelliJ-Tool")
--- intellj-plugin/src/main/actions/SimpleAction.java
+++ intellj-plugin/src/main/actions/SimpleAction.java
@@ -2,21 +2,13 @@ package main.actions;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DataKeys;
 import com.intellij.openapi.ui.MessageType;
 import com.intellij.openapi.ui.popup.Balloon;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.openapi.wm.StatusBar;
-import com.intellij.openapi.wm.WindowManager;
 import com.intellij.ui.awt.RelativePoint;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
 
 import javax.swing.*;
-import java.awt.*;
-import java.util.concurrent.Callable;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 /**
